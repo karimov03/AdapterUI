26aab7cd25d0d3a7a2887f0e7f9f2bbfcda5a6c9
karimov03 <123530135+karimov03@users.noreply.github.com>
2023-02-05 22:36:24 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/adapterui/MainActivity.kt
+++ app/src/main/java/com/example/adapterui/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.adapterui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.adapterui.Adpters.Adapter
+import com.example.adapterui.databinding.ActivityItemBinding
+import com.example.adapterui.databinding.ActivityMainBinding
+import com.example.adapterui.utils.User
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val list=ArrayList<User>()
+    private  lateinit var userAdapter: Adapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        userAdapter= Adapter(list)
+
+
+
+
+        binding.apply {
+
+            rv1.adapter=userAdapter
+            btnSave.setOnClickListener {
+                val user=User(
+                    tvName.text.toString(),
+                    tvAge.text.toString()
+                )
+                list.add(user)
+                userAdapter.notifyDataSetChanged()
+            }
+
+
+
+
+
+
+//            Finish
+        }
     }
 }
